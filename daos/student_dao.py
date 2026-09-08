cd0671010dedec4f6b88ab42ac04b18b7b2c2f83
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-08 15:25:51 +0200

    Implémentation des méthodes read et delete pour student_dao

--- daos/student_dao.py
+++ daos/student_dao.py
@@ -46,14 +46,26 @@ class StudentDao(Dao[Student]):
             Dao.connection.rollback()
         return 0
 
-    # TODO Méthode à implémenter
     def read(self, student_nbr: int) -> Optional[Student]:
-        """Renvoit le student correspondant à l'entité dont l'id est student_nbr
+        """Renvoie le student correspondant à l'entité dont l'id est student_nbr
            (ou None s'il n'a pu être trouvé)"""
-        return None
+        student: Optional[Student]
+
+        with Dao.connection.cursor() as cursor:
+            sql = "SELECT student.student_nbr, student.id_person, person.first_name, person.last_name, person.age, person.id_address FROM student JOIN person ON student.id_person = person.id_person WHERE student_nbr=%s"
+            cursor.execute(sql, (student_nbr,))
+            record = cursor.fetchone()
+        if record is not None:
+            student = Student(record['first_name'], record['last_name'], record['age'])
+            student.student_nbr = record['student_nbr']
+        else:
+            student = None
+
+        return student
+
 
     def read_all(self):
-        """Renvoit la liste des cours """
+        """Renvoie la liste des cours """
         with Dao.connection.cursor() as cursor:
             sql = "SELECT student.student_nbr, student.id_person, person.first_name, person.last_name, person.age, person.id_address FROM student JOIN person ON student.id_person = person.id_person "
             cursor.execute(sql)
@@ -81,12 +93,60 @@ class StudentDao(Dao[Student]):
 
         return True
 
-    # TODO Méthode à implémenter
     def delete(self, student: Student) -> bool:
         """Supprime en BD l'entité Student correspondant à student
 
         :param student: student dont l'entité Student correspondante est à supprimer
         :return: True si la suppression a pu être réalisée
         """
+        try:
+            with Dao.connection.cursor() as cursor:
+                # On va d'abord récupérer l'id de la personne qui correspond à cet étudiant
+                sql = "SELECT id_person FROM student WHERE student_nbr = %s"
+                cursor.execute(sql, (student.student_nbr,))
 
+                result = cursor.fetchone()
+
+                if result is None:
+                    print("Erreur lors de la récupération de l'id de la personne correspondant à cet étudiant")
+                    return False
+
+                id_person = result['id_person']
+                # print("Id personne : ", id_person)
+
+                # On va supprimer l'élève
+                sql = "DELETE FROM student WHERE student_nbr = %s "
+                cursor.execute(sql, (student.student_nbr,))
+
+                print(sql)
+                print("student_nbr :", student.student_nbr)
+                print("id_person :", id_person)
+                print("DELETE student :", cursor.rowcount)
+
+                # cursor.rowcount = nombre de lignes qui ont été affectées par cette requête. On attend une seule (une suppression).
+                if cursor.rowcount != 1:
+                    #Si il y a erreur on remet la base dans l'état ou elle était
+                    Dao.connection.rollback()
+                    return False
+
+                # On va supprimer la personne qui correspond à l'enseignant
+                sql = "DELETE FROM person WHERE id_person = %s"
+                cursor.execute(sql, (id_person,))
+
+                print(sql)
+                print("DELETE person :", cursor.rowcount)
+
+                # cursor.rowcount = nombre de lignes qui ont été affectées par cette requête. On attend une seule (une suppression).
+                if cursor.rowcount != 1:
+                    # Si il y a erreur on remet la base dans l'état ou elle était
+                    Dao.connection.rollback()
+                    return False
+
+                # Si c'est ok, on commit
+                Dao.connection.commit()
+                print("COMMIT effectué")
+        except Exception as e:
+            print(f"Exception : {e}")
+            Dao.connection.rollback()
+            return False
         return True
